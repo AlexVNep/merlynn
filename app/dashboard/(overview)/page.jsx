232535eb97f81057d887debac75149403d6fd04c
Alex <alexvnep@gmail.com>
2024-11-19 16:45:29 +0200

    feat: Create a dashboard page to hold general info

---
+++ app/dashboard/(overview)/page.jsx
@@ -0,0 +1,13 @@
+export default async function Page() {
+  return (
+    <main>
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+      <div>
+        <h2>List of Decisions</h2>
+        {/* <Table></Table> */}
+      </div>
+    </main>
+  );
+}
