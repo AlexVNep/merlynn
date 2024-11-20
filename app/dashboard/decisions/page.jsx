0881f2abe6232dd9c17da15ff4fb5d60dc1d01ee
Alex <alexvnep@gmail.com>
2024-11-20 09:18:41 +0200

    feat: add page to display all decisions

---
+++ app/dashboard/decisions/page.jsx
@@ -0,0 +1,16 @@
+export default async function Page() {
+  return (
+    <main>
+      <div>
+        <h1>Decisions History</h1>
+      </div>
+      <div>
+        <h2>FULL List of Decisions</h2>
+        {/* Show 10 decisions at a time taken from the database
+          Add navigation buttons to be taken to FORM, HOME(DASHBOARD), DECISIONS and CREATEUSER
+          */}
+        {/* <Table></Table> */}
+      </div>
+    </main>
+  );
+}
