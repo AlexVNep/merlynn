6a156997b7e044cb94b61b4942e27b3625ef74e4
Alex <alexvnep@gmail.com>
2024-11-20 09:20:44 +0200

    refactor: move form to it's own page under dashboard

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,1 @@
-import InputForm from "./ui/form/InputForm";
-import { getModel } from "./lib/action";
 
-export default async function Home() {
-  const data = await getModel();
-  const metadata = data;
-  return (
-    <>
-      <h1>Home</h1>
-      <InputForm formData={metadata}></InputForm>
-    </>
-  );
-}
