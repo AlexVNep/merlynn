0b17b8765102d09c007420e25a52a0f0f2f76338
Alex <alexvnep@gmail.com>
2024-11-25 10:41:03 +0200

    feat: Add signup form comp

---
+++ app/ui/signup-form.jsx
@@ -0,0 +1,135 @@
+"use-client";
+
+import {
+  UserIcon,
+  AtSymbolIcon,
+  KeyIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { useActionState } from "react";
+import Link from "next/link";
+import { createUserAction } from "../lib/action";
+// import { authenticate } from "@/app/lib/actions";
+
+export default function SignupForm() {
+  const initialState = {
+    data: null,
+  };
+  const [state, formAction, errorMessage, isPending] = useActionState(
+    createUserAction,
+    initialState
+  );
+
+  console.log(state);
+
+  return (
+    <div>
+      <form action={formAction} className="space-y-3">
+        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+          <h1 className="mb-3 text-black text-2xl">
+            Please sign up to continue.
+          </h1>
+          <div className="w-full">
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="email"
+              >
+                Username
+              </label>
+              <div className="relative">
+                <input
+                  className="peer text-black block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="email"
+                  type="text"
+                  name="username"
+                  placeholder="Enter your username"
+                />
+                <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+              {state?.errors?.username && (
+                <p className="text-red-500">{state.errors.username}</p>
+              )}
+            </div>
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  className="peer text-black block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email address"
+                />
+                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+              {state?.errors?.email && (
+                <p className="text-red-500">{state.errors.email}</p>
+              )}
+            </div>
+            <div className="mt-4">
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  className="peer text-black block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                />
+                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+              {state?.errors?.password && (
+                <div className="text-red-500">
+                  <p>Password must:</p>
+                  <ul>
+                    {state.errors.password.map((error) => (
+                      <li key={error}>- {error}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="mt-4 w-full text-black"
+            aria-disabled={isPending}
+          >
+            Sign Up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          </button>
+          <div
+            className="flex h-8 items-end space-x-1"
+            aria-live="polite"
+            aria-atomic="true"
+          >
+            {errorMessage && (
+              <>
+                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                <p className="text-sm text-red-500">{errorMessage}</p>
+              </>
+            )}
+          </div>
+        </div>
+      </form>
+      <div className="text-center text-gray-500 mt-4">- OR -</div>
+      <Link
+        href="/login"
+        className="block text-center text-blue-500 hover:underline mt-2"
+      >
+        Login with an existing account
+      </Link>
+    </div>
+  );
+}
