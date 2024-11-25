9036d1807c0dff3347f1319c9bfd49875e3e8752
Alex <alexvnep@gmail.com>
2024-11-25 10:43:27 +0200

    add to do

--- app/dashboard/(overview)/page.jsx
+++ app/dashboard/(overview)/page.jsx
@@ -6,6 +6,9 @@ export default async function Page() {
       </div>
       <div>
         <h2>List of Decisions</h2>
+        {/* Only add upto 5 decisions taken from the database
+        Add navigation buttons to be taken to FORM, HOME(DASHBOARD), DECISIONS and CREATEUSER
+        */}
         {/* <Table></Table> */}
       </div>
     </main>
