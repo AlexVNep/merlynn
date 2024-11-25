d84ae51c1f16c852635110661df0e6df7a71f7dd
Alex <alexvnep@gmail.com>
2024-11-25 10:43:08 +0200

    feat: add createUser action to add data to db

--- app/lib/action.js
+++ app/lib/action.js
@@ -3,6 +3,8 @@ import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import mongoose from "mongoose";
 import TomDecicions from "../models/Decision";
+import User from "../models/User";
+import bcrypt from "bcryptjs";
 
 main().catch((err) => console.log(err));
 
@@ -152,6 +154,58 @@ async function createDecision(data) {
       drinksToday: data.data.attributes.input.INPUTVAR9,
     },
   });
+}
 
-  console.log(test);
+export async function createUserAction(state, formData) {
+  const SignupFormSchema = z.object({
+    username: z
+      .string()
+      .min(2, { message: "Name must be at least 2 characters long." })
+      .trim(),
+    email: z.string().email({ message: "Please enter a valid email." }).trim(),
+    password: z
+      .string()
+      .min(8, { message: "Be at least 8 characters long" })
+      .regex(/[a-zA-Z]/, { message: "Contain at least one letter." })
+      .regex(/[0-9]/, { message: "Contain at least one number." })
+      .regex(/[^a-zA-Z0-9]/, {
+        message: "Contain at least one special character.",
+      })
+      .trim(),
+  });
+
+  const validatedFields = SignupFormSchema.safeParse({
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Register.",
+    };
+  }
+
+  const { username, email, password } = validatedFields.data;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return new Error("Email is already in use", { status: 400 });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const data = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  const user = await User.findOne({ username: data.username });
+
+  if (!user) {
+    return {
+      message: "An error occurred while creating your account.",
+    };
+  }
 }
