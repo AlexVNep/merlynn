5ed97567f53e7a65348501f13788392ef002fc0f
Alex <alexvnep@gmail.com>
2024-12-05 14:15:07 +0200

    Add download link

--- app/ui/dashboard/nav-links.jsx
+++ app/ui/dashboard/nav-links.jsx
@@ -6,6 +6,7 @@ import {
   DocumentDuplicateIcon,
   DocumentPlusIcon,
   ArrowUpIcon,
+  ArrowDownIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -32,6 +33,11 @@ const links = [
     name: "Batch Upload ",
     href: "/dashboard/batchupload",
     icon: ArrowUpIcon,
+  },
+  {
+    name: "Batch Download ",
+    href: "/dashboard/batchdownload",
+    icon: ArrowDownIcon,
   },
 ];
 
